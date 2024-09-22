c5e8c50b1ccc9d073bf45be39bc3e212dd424874
theKAOSSphere <thekaossphere@gmail.com>
2024-09-22 21:13:39 +0530

    changed paths and stuff

--- obl-lv2/obl-lv2.mk
+++ obl-lv2/obl-lv2.mk
@@ -1,34 +1,34 @@
 ######################################
 #
-# loopor-lv2
+# obl-lv2
 #
 ######################################
 
 # where to find the source code - locally in this case
-LOOPOR_LV2_SITE_METHOD = local
-LOOPOR_LV2_SITE = $($(PKG)_PKGDIR)/
+OBL_LV2_SITE_METHOD = local
+OBL_LV2_SITE = $($(PKG)_PKGDIR)/
 
 # even though this is a local build, we still need a version number
 # bump this number if you need to force a rebuild
-LOOPOR_LV2_VERSION = 5
+OBL_LV2_VERSION = 1
 
 # dependencies (list of other buildroot packages, separated by space)
-LOOPOR_LV2_DEPENDENCIES =
+OBL_LV2_DEPENDENCIES =
 
 # LV2 bundles that this package generates (space separated list)
-LOOPOR_LV2_BUNDLES = loopor.lv2
+OBL_LV2_BUNDLES = obl.lv2
 
 # call make with the current arguments and path. "$(@D)" is the build directory.
-LOOPOR_LV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/source
+OBL_LV2_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/source
 
 # build command
-define LOOPOR_LV2_BUILD_CMDS
-	$(LOOPOR_LV2_TARGET_MAKE)
+define OBL_LV2_BUILD_CMDS
+	$(OBL_LV2_TARGET_MAKE)
 endef
 
 # install command
-define LOOPOR_LV2_INSTALL_TARGET_CMDS
-	$(LOOPOR_LV2_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+define OBL_LV2_INSTALL_TARGET_CMDS
+	$(OBL_LV2_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
 endef
 
 
